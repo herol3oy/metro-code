b474c945811e686d7185f7f61a850bb5fdac18ff
demah <hamedsedighi@gmail.com>
2023-08-09 15:24:34 +0200

    sort blog posts

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,24 @@ import { formatDate } from '@/utils/format-date'
 import { getAllPosts } from '@/utils/post'
 import Link from 'next/link'
 
+interface BlogPost {
+  title: string
+  date: string
+  subtitle: string
+  slug: string
+}
+
 const Home = async () => {
-  const postMetadata = await getAllPosts()
+  const postMetadata: BlogPost[] = await getAllPosts()
+  const sortedPosts = postMetadata.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+  )
 
   return (
     <div className="flex flex-col gap-5">
       <AboutMe />
       <h1 className="text-2xl font-bold">Recent notes</h1>
-      {postMetadata.map(({ slug, title, date }) => (
+      {sortedPosts.map(({ slug, title, date }) => (
         <div className="flex justify-between" key={slug}>
           <Link
             className="w-fit transition-all duration-500 hover:text-gray-600"
